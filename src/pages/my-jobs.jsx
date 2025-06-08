b6b0fc3527a2be162a311645d9dc0e1cedc6b8b2
Harsh Pawar <harsh.pawar.ug23@nsut.ac.in>
2025-06-08 22:26:29 +0530

    Deployment 1

--- src/pages/my-jobs.jsx
+++ src/pages/my-jobs.jsx
@@ -14,11 +14,11 @@ const MyJobs = () => {
   return (
     <div>
       <h1 className="gradient-title font-extrabold text-5xl sm:text-7xl text-center pb-8">
-        {user?.unsafeMetadata?.role !== "candidate"
+        {user?.unsafeMetadata?.role === "candidate"
           ? "My Applications"
           : "My Jobs"}
       </h1>
-      {user?.unsafeMetadata?.role !== "candidate" ? (
+      {user?.unsafeMetadata?.role === "candidate" ? (
         <CreatedApplications />
       ) : (
         <CreatedJobs />
